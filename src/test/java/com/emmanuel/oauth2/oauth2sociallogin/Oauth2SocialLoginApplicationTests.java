ed6fa7b0ad3dba9bc811ff5e6ccdd57b54dba2ab
OBUSEHEMMANUEL <obusehemmanuel208@gmail.com>
2023-05-24 12:30:40 +0100

    oauth2Login

---
+++ src/test/java/com/emmanuel/oauth2/oauth2sociallogin/Oauth2SocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emmanuel.oauth2.oauth2sociallogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2SocialLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
